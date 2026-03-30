732f8212702981c9547c056dbea3092627e097e5
zyad2012104-maker <zyad2012104@gmail.com>
2026-03-30 04:03:18 +0200

    Add files via upload

--- common.js
+++ common.js
@@ -1,11 +1,5 @@
 // ========== إعدادات JSONBin ==========
-const CONFIG = {
-    JSONBIN: {
-        BASE_URL: "https://api.jsonbin.io/v3/b/",
-        BIN_ID: "69c9835736566621a85cb01f",
-        MASTER_KEY: "$2a$10$5X1fbgOhCiGV23rKGUkLLuhD/a1eIHNuKwvtNzKwu3W7KT8CGpaG."
-    }
-};
+
 // ========== البيانات ==========
 let apps = [];
 let jsonbinReady = false;
